28d0307791d2656c5949b36da82d17211f0a3f6e
Bearww <wu810517@gmail.com>
2016-06-27 22:35:05 +0800

    Basic function: local login & mouse control

---
+++ app/src/main/java/com/test/wu/remotetest/Constants.java
@@ -0,0 +1,13 @@
+package com.test.wu.remotetest;
+
+/**
+ * Created by Wu on 2016/6/25.
+ */
+public class Constants {
+    public static final String SERVER_IP = "192.168.137.74";
+    public static final int SERVER_PORT = 6060;
+    public static final String PLAY= "play";
+    public static final String NEXT= "next";
+    public static final String PREVIOUS= "previous";
+    public static final String MOUSE_LEFT_CLICK= "left_click";
+}
